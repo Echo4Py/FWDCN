6c9ab83e5a511598b914b060e674fa18ba0698c2
Echo4Py <georgechii@outlook.com>
2016-07-04 20:57:42 +0800

    review chapter2

--- part1/viewfn.py
+++ part1/viewfn.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('127.0.0.1')
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {:s}</p>'.format(user_agent)
+    
+@app.route('/index')
+def index2():
+    return '<h1>Hello World!</h1>'
+    
+@app.route('/br')
+def index3():
+    return '<h1>Bad Request</h1>', 400
+    
+@app.route('/coki')
+def index4():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h1>Hello, {:s}!</h1>'.format(user.name)
+@app.route('/rd')
+def index5():
+    return redirect('/user/George')
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {:s}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
