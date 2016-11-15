7fd607fce82659d1c75c0aed60e6ab2f1cd3fc6e
George <babyfor@qq.com>
2016-11-15 10:08:32 +0800

    backup re

---
+++ render/find_something_in_txt.py
@@ -0,0 +1,9 @@
+﻿# -*-coding:utf-8 -*-
+import re
+print "你要查找什么内容？请输入："
+pattern = raw_input()
+text = "今天双11我给儿子买了2个玩具在淘宝上。"
+match = re.search(pattern, text)
+s = match.start()
+e = match.end()
+print '发现 "%s"\n在这句话中 "%s"\n %d 到 %d 的位置->(%s)' % (match.re.pattern, match.string, s, e, text[s:e])
